535e59f17f8f06cdbd04541bd2e52dae94a7670c
infiaite <jamesli8624@hotmail.com>
2024-06-16 00:24:02 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,84 +8,9 @@
           <n-text :depth="3">汇聚全网热点，热门尽览无余</n-text>
         </div>
       </div>
-      <div class="current-time" v-if="store.timeData">
-        <n-text class="time">{{ store.timeData.time.text }}</n-text>
-        <n-text class="date" :depth="3">
-          {{
-            store.timeData.lunar.GanZhiYear +
-            "年 " +
-            store.timeData.lunar.text +
-            " " +
-            store.timeData.time.weekday
-          }}
-        </n-text>
-      </div>
-      <div class="current-time" v-else>
-        <n-text class="time">时间获取中</n-text>
-      </div>
-      <div class="controls">
-        <n-space justify="end">
-          <n-popover v-if="showRefresh">
-            <template #trigger>
-              <n-button secondary strong round @click="router.go(0)">
-                <template #icon>
-                  <n-icon :component="Refresh" />
-                </template>
-              </n-button>
-            </template>
-            刷新页面
-          </n-popover>
-          <n-popover>
-            <template #trigger>
-              <n-button
-                secondary
-                strong
-                round
-                @click="
-                  store.setSiteTheme(
-                    store.siteTheme === 'light' ? 'dark' : 'light'
-                  )
-                "
-              >
-                <template #icon>
-                  <n-icon
-                    :component="store.siteTheme === 'light' ? Moon : SunOne"
-                  />
-                </template>
-              </n-button>
-            </template>
-            {{ store.siteTheme === "light" ? "深色模式" : "浅色模式" }}
-          </n-popover>
-          <n-popover>
-            <template #trigger>
-              <n-button secondary strong round @click="router.push('/setting')">
-                <template #icon>
-                  <n-icon :component="SettingTwo" />
-                </template>
-              </n-button>
-            </template>
-            全局设置
-          </n-popover>
-        </n-space>
-      </div>
-      <div class="mobile">
-        <n-dropdown
-          :options="menuOptions"
-          size="large"
-          trigger="click"
-          placement="bottom-end"
-          @select="menuOptionsSelect"
-        >
-          <n-button secondary strong round>
-            <template #icon>
-              <n-icon :component="HamburgerButton" />
-            </template>
-          </n-button>
-        </n-dropdown>
-      </div>
-    </section>
-  </n-card>
-</template>
+      
+
+    
 
 <script setup>
 import {
@@ -95,7 +20,6 @@ import {
   SettingTwo,
   HamburgerButton,
 } from "@icon-park/vue-next";
-import { getCurrentTime } from "@/utils/getTime.js";
 import { mainStore } from "@/store";
 import { NText, NIcon } from "naive-ui";
 import { useRouter } from "vue-router";
@@ -105,95 +29,10 @@ const store = mainStore();
 const timeInterval = ref(null);
 const showRefresh = ref(false);
 
-// 移动端时间模块
-const timeRender = () => {
-  return h(
-    "div",
-    {
-      style: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        padding: "6px 18px",
-      },
-    },
-    [
-      h(NText, null, {
-        default: () =>
-          store.timeData ? store.timeData.time.text : "时间获取失败",
-      }),
-      h(
-        NText,
-        { depth: 3, style: "font-size: 12px" },
-        {
-          default: () =>
-            store.timeData
-              ? store.timeData.lunar.GanZhiYear +
-                "年 " +
-                store.timeData.lunar.text +
-                " " +
-                store.timeData.time.weekday
-              : "日期获取失败",
-        }
-      ),
-    ]
-  );
-};
 
-// 移动端菜单
-const menuOptions = [
-  {
-    key: "header",
-    type: "render",
-    render: timeRender,
-  },
-  {
-    key: "header-divider",
-    type: "divider",
-  },
-  {
-    label: "刷新页面",
-    key: "refresh",
-    icon: () => {
-      return h(NIcon, null, {
-        default: () => h(Refresh),
-      });
-    },
-  },
-  {
-    label: () => {
-      return h(NText, null, {
-        default: () => (store.siteTheme === "light" ? "深色模式" : "浅色模式"),
-      });
-    },
-    key: "changeTheme",
-    icon: () => {
-      return h(NIcon, null, {
-        default: () => (store.siteTheme === "light" ? h(Moon) : h(SunOne)),
-      });
-    },
-  },
-  {
-    label: "全局设置",
-    key: "setting",
-    icon: () => {
-      return h(NIcon, null, {
-        default: () => h(SettingTwo),
-      });
-    },
-  },
-];
 
-// 移动端下拉菜单点击事件
-const menuOptionsSelect = (val) => {
-  if (val === "refresh") {
-    router.go(0);
-  } else if (val === "changeTheme") {
-    store.setSiteTheme(store.siteTheme === "light" ? "dark" : "light");
-  } else if (val === "setting") {
-    router.push("/setting");
-  }
-};
+
+
 
 // 监听路由参数变化
 watch(

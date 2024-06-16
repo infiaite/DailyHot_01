c36b6652f6dfee624fe31562c8d1aba52b872d55
infiaite <jamesli8624@hotmail.com>
2024-06-16 20:00:11 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -69,7 +69,7 @@
         
           <n-popover>
             <template #trigger>
-              <n-button secondary strong round @click="jumpLink('http://mail.hyermall.com/')">        
+              <n-button secondary strong round @click="jumpLink('http://www.hyermall.com/')">        
                 <template #icon>
                   <n-icon :component="Refresh" />
                 </template>

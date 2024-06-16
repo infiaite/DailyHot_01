7abde9aed8e1bdc094e6d48e9cc20a5cf0463758
infiaite <jamesli8624@hotmail.com>
2024-06-16 14:20:10 +0800

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -69,7 +69,7 @@
         
           <n-popover>
             <template #trigger>
-                   <n-button secondary strong round @click="() => this.$router.push('www.hyermall.com')">
+                   <n-button secondary strong round @click="() => this.$router.push('http://www.hyermall.com')">
               
                 <template #icon>
                   <n-icon :component="SettingTwo" />

ce2acbffeae84299ca4212a5c7fa38a27388b43a
infiaite <jamesli8624@hotmail.com>
2024-06-16 15:03:40 +0800

    Update hyer.vue

--- src/views/hyer.vue
+++ src/views/hyer.vue
@@ -1,1 +1,15 @@
+<template>
+  <n-button secondary strong round @click="goToHyermall">
+    Go to HyerMall
+  </n-button>
+</template>
 
+<script>
+export default {
+  methods: {
+    goToHyermall() {
+      this.$router.push('http://www.hyermall.com');
+    }
+  }
+}
+</script>
